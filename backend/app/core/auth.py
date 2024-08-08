72dd1c8c8b89aa015a1cf04de8d711558dec0c73
Ivan Kwong <ivankwongtszfung@gmail.com>
2024-08-08 14:00:34 -0400

    remove dep put into auth db

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -1,20 +1,10 @@
-from collections.abc import Generator
 from typing import Annotated
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from sqlmodel import Session
 
 from app.core.config import settings
-from app.core.db import engine
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_VERSION_PREFIX}/token")
 
-
-def get_db() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
-
-
-SessionDep = Annotated[Session, Depends(get_db)]
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,5 +1,18 @@
+from collections.abc import Generator
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy import create_engine
+from sqlmodel import Session
 
 from app.core.config import settings
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
